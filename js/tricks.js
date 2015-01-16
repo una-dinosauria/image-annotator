86513e35eebc4fd78a6a955b79b513930e45b3ac
Julieta <julm@cs.ubc.ca>
2015-01-16 02:00:37 -0800

    finishing template

--- js/tricks.js
+++ js/tricks.js
@@ -1,21 +1,27 @@
 
 // This is where our main code will be.
 
-// The image that we are looking at.
-var current_image = 1;
+var NIMAGES = 111;
+
+var current_image = 1; // the image we are looking at.
 
 $('#fwd_bttn').click(function () {
+	if (current_image === NIMAGES) { return; }
 
-	// Update the current image variable
-	current_image = current_image + 1;
-
-	// This is the file that we want to load
-	fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]);
-
-	// Actually load the image.
-	$('#to_annotate').attr('src', fname);
-
-	// Update the text at the top
-	$('#info-text').text(fname);
+	current_image = current_image + 1; // Update image variable.
+	var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
+	$('#to_annotate').attr('src', fname); // Actually load the image.
+	var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
+	$('#info-text').text( new_text ); // Update the text at the top
 });
 
+
+$('#back_bttn').click( function() {
+	if (current_image === 1) {return;}
+
+	current_image = current_image - 1;
+	var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
+	$('#to_annotate').attr('src', fname); // Actually load the image.
+	var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
+	$('#info-text').text( new_text ); // Update the text at the top
+});
