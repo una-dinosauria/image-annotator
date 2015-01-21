774e2fb3b9da900cec26b55c1d7ed859031b7f7d
Umang <umang.18sep@gmail.com>
2015-01-21 12:39:43 +0530

    Added play, pause and stop buttons

--- js/tricks.js
+++ js/tricks.js
@@ -2,26 +2,69 @@
 // This is where our main code will be.
 
 var NIMAGES = 111;
+var play                // this is set to stop the playing when paused or stopped
+var current_image = 1;  // the image we are looking at.
+$('#pause_bttn').hide() // since we are not playing initially
 
-var current_image = 1; // the image we are looking at.
+
+//updates the frame details.
+function update_image(){
+    var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
+    $('#to_annotate').attr('src', fname); // Actually load the image.
+    var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
+    $('#info-text').text( new_text ); // Update the text at the top
+} 
+
+//set frame no to frame_no
+function set_frame(frame_no){
+    current_image  = frame_no;
+    update_image();
+}
+
+// moves ahead by no_of_frame --- can take negative no. and go back
+function advance_frame(no_of_frame){
+    var temp_current_image = current_image + no_of_frame;
+    if (temp_current_image > NIMAGES){
+        current_image = NIMAGES;
+    }else if  (temp_current_image < 1){
+        current_image = 1;
+    }else{
+        current_image = temp_current_image;
+    }
+    update_image();
+    return ; 
+}
+
 
 $('#fwd_bttn').click(function () {
-	if (current_image === NIMAGES) { return; }
-
-	current_image = current_image + 1; // Update image variable.
-	var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
-	$('#to_annotate').attr('src', fname); // Actually load the image.
-	var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
-	$('#info-text').text( new_text ); // Update the text at the top
+    advance_frame(1);
 });
 
+//currently playing speed is set to 10fps
+$('#play_bttn').click(function () {
+    $('#play_bttn').toggle();
+    $('#pause_bttn').toggle();
+    play = window.setInterval(function() {
+        advance_frame(1);
+        if (current_image == NIMAGES){
+            window.clearInterval(play);
+        }
+    },100);
+});
+
+$('#pause_bttn').click(function () {
+    $('#play_bttn').toggle();
+    $('#pause_bttn').toggle();
+    window.clearInterval(play);
+});
+
+$('#stop_bttn').click(function () {
+    window.clearInterval(play);
+    $('#play_bttn').show();
+    $('#pause_bttn').hide();
+    set_frame(1);
+});
 
 $('#back_bttn').click( function() {
-	if (current_image === 1) {return;}
-
-	current_image = current_image - 1;
-	var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
-	$('#to_annotate').attr('src', fname); // Actually load the image.
-	var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
-	$('#info-text').text( new_text ); // Update the text at the top
+    advance_frame(-1);
 });
