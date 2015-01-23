c77996d728a8cb8207cd6fe79f14d461d466fca5
Julieta <julm@cs.ubc.ca>
2015-01-23 15:53:04 -0800

    Changing path of image

--- js/tricks.js
+++ js/tricks.js
@@ -1,26 +1,28 @@
 
 // This is where our main code will be.
 
-var NIMAGES = 111;
+var NIMAGES = 100;
 var play                // this is set to stop the playing when paused or stopped
 var current_image = 1;  // the image we are looking at.
 $('#pause_bttn').hide() // since we are not playing initially
+
+var img_path = 'imgs/undercam/cam1logfile1_%05.jpeg';
 
 //preload images
 var annotate_images={};
 for (i=0;i<NIMAGES;i++){
     annotate_images[i] = new Image();
-    var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [i+1]); // Name of the image file.
+    var fname = sprintf( img_path, [i+1] ); // Name of the image file.
     annotate_images[i].src = fname
 }
 
 //updates the frame details.
 function update_image(){
-    var fname = sprintf('imgs/cam1logfile1_%05d.jpeg', [current_image]); // Name of the image file.
+    var fname = sprintf( img_path, [current_image]); // Name of the image file.
     $('#to_annotate').attr('src', fname); // Actually load the image.
     var new_text = sprintf('You are currently on frame %d / %d', current_image, NIMAGES);
     $('#info-text').text( new_text ); // Update the text at the top
-} 
+}
 
 //set frame no to frame_no
 function set_frame(frame_no){
@@ -39,7 +41,7 @@ function advance_frame(no_of_frame){
         current_image = temp_current_image;
     }
     update_image();
-    return ; 
+    return ;
 }
 
 
